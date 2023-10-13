f9298596cc5e32260dd24ce794ef30021d47f536
Shao <leo21604212@gmail.com>
2023-10-13 12:48:40 -0400

    6 47y354

--- dom.js
+++ dom.js
@@ -6,5 +6,7 @@ DOMSelectors = {
 
 DOMSelectors.form.addEventListener("submit", function (event){
     event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach(
+        (header) => (header.textContent = DOMSelectors.firstName.value)
+    );
 });
