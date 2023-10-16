3b71e54e7a13d3f9da9faa72f6c01c5dea0fe3a3
Shao <leo21604212@gmail.com>
2023-10-16 13:12:42 -0400

    rerwewr43!43143423 432342

--- dom.js
+++ dom.js
@@ -10,3 +10,18 @@ DOMSelectors.form.addEventListener("submit", function (event){
         (header) => (header.textContent = DOMSelectors.firstName.value)
     );
 });
+
+const insert = document.querySelector("#insert");
+insert.addEventListener("click", () => {
+  const subject = document.querySelector("#subject");
+  const positionSelect = document.querySelector("#position");
+  subject.insertAdjacentHTML(
+    positionSelect.value,
+    "<strong>inserted text</strong>",
+  );
+});
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  document.location.reload();
+});
