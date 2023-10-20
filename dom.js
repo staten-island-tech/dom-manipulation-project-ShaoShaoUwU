31a4edf2489435063f0d49865773ffdb75394d0e
Shao <leo21604212@gmail.com>
2023-10-20 12:55:18 -0400

    32423

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-DOMSelectors = {
+/* DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
@@ -9,6 +9,17 @@ DOMSelectors.form.addEventListener("submit", function (event){
     DOMSelectors.h2s.forEach(
         (header) => (header.textContent = DOMSelectors.firstName.value)
     );
-});
+}); 
+ */
 
-+
+function card(Champ){
+    document.querySelector(".gallery").insertAdjacentElement(
+        "beforend",
+        `<div class="card">
+        <h2 class="card-title">${Champ.name}</h2>
+        <div>`
+    );
+};
+DOMSelectors.form.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
